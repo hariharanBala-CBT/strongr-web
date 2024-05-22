63a14c21d33883b4d60e0fe5212fd958fcee4c2c
Rugved <rugvedm@cloverbridgetech.com>
2024-05-22 12:46:56 +0530

    settings for local & production

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -7,10 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# application = get_wsgi_application()
+
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/opt/strongr-web/backend/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+try:
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
+except:
+    pass
+
+# Fix django closing connection to MemCachier after every request (#11331)
+from django.core.cache.backends.memcached import BaseMemcachedCache
+BaseMemcachedCache.close = lambda self, **kwargs: None
