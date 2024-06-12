82296cff4142758e03e4ae7fe59b9a61886c263d
TharunCBT <160119197+TharunCBT@users.noreply.github.com>
2024-06-12 10:10:34 +0530

    user signup validation (#4)
    * user signup validation
    
    * merge
    
    * fix-nit
    
    * style-fix

--- backend/frontend/src/store.js
+++ backend/frontend/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 import {
   clubListReducer,
   areaListReducer,
@@ -90,7 +90,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  (applyMiddleware(...middleware)),
+  composeWithDevTools((applyMiddleware(...middleware))),
 );
 
 export default store;
